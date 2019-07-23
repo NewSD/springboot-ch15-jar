68ebc7d2dc376df86f72159fa4abe10e90315eb3
18300603955 <oceaniu@163.com>
2019-07-23 14:45:33 +0800

    init

---
+++ src/main/java/com/niu/SpringbootCh15JarApplication.java
@@ -0,0 +1,13 @@
+package com.niu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCh15JarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCh15JarApplication.class, args);
+	}
+
+}
